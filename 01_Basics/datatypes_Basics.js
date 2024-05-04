0ed81288a205475b364df8fb54eb010b8166ef31
yaswanth3733 <154798906+yaswanth3733@users.noreply.github.com>
2024-05-04 05:57:41 +0000

    datatypes commit done

---
+++ 01_Basics/datatypes_Basics.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log(3+3) ; 
+console.log("Hrutesh kumar");
+
+let Name = "Hrutesh"
+let age = 20
+let isLoggedIn = false
+
+console.table([Name,age,isLoggedIn])
+console.log(typeof Name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
