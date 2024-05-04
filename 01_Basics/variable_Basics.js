ced6a8d62eadd3033f84bc8079c353325f92ab56
yaswanth3733 <154798906+yaswanth3733@users.noreply.github.com>
2024-05-04 05:57:19 +0000

    variables commit done

---
+++ 01_Basics/variable_Basics.js
@@ -0,0 +1,12 @@
+const account_Id = 121212
+var account_Name = "Yaswanth"
+let account_Username = "Yash"
+account_Password = "3730"
+
+// account_Id = 121 it is not allowed to change the value of const
+
+account_Name = "Gowtham"
+account_Username = "Goww"
+account_Password = "0513"
+
+console.table([account_Id , account_Name , account_Username , account_Password])
